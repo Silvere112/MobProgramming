ab0cb3302b6817ff3cf798c4d49a0aad6c8ee8ca
Silvère Marie <silvere.marie@external.grtgaz.com>
2022-02-09 17:24:05 +0100

    first commit

---
+++ src/main/java/grtgaz/rio/mobprogramming/MobProgrammingApplication.java
@@ -0,0 +1,13 @@
+package grtgaz.rio.mobprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobProgrammingApplication.class, args);
+    }
+
+}
